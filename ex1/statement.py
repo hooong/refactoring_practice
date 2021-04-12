5b11a57ed611d13efdc63426225dc71fd47444ea
hooongsj <tjrwns0529@gmail.com>
2021-04-13 00:42:17 +0900

    delete play from render_plain_text

--- ex1/statement.py
+++ ex1/statement.py
@@ -4,36 +4,37 @@ import copy
 def statement(invoice, plays):
     def enrich_performance(a_performance):
         result = copy.copy(a_performance)
-        return result
-
-    statement_data = {}
-    statement_data['customer'] = invoice['customer']
-    statement_data['performances'] = list(map(enrich_performance, invoice['performances']))
-    return render_plain_text(statement_data, invoice, plays)
-
-
-def render_plain_text(data, invoice, plays):
-    def amount_for(a_performance):
-        if play_for(a_performance)['type'] == 'tragedy':
-            result = 40000
-            if a_performance["audience"] > 30:
-                result += 1000 * (a_performance["audience"] - 30)
-        elif play_for(a_performance)['type'] == 'comedy':
-            result = 30000
-            if a_performance["audience"] > 20:
-                result += 10000 + 500 * (a_performance["audience"] - 20)
-            result += 300 * a_performance["audience"]
-        else:
-            raise Exception(f'알 수 없는 장르 : {play_for(a_performance)["type"]}')
+        result['play'] = play_for(result)
         return result
 
     def play_for(a_performance):
         return plays[a_performance["playID"]]
 
+    statement_data = {}
+    statement_data['customer'] = invoice['customer']
+    statement_data['performances'] = list(map(enrich_performance, invoice['performances']))
+    return render_plain_text(statement_data)
+
+
+def render_plain_text(data):
+    def amount_for(a_performance):
+        if a_performance['play']['type'] == 'tragedy':
+            result = 40000
+            if a_performance["audience"] > 30:
+                result += 1000 * (a_performance["audience"] - 30)
+        elif a_performance['play']['type'] == 'comedy':
+            result = 30000
+            if a_performance["audience"] > 20:
+                result += 10000 + 500 * (a_performance["audience"] - 20)
+            result += 300 * a_performance["audience"]
+        else:
+            raise Exception(f'알 수 없는 장르 : {a_performance["play"]["type"]}')
+        return result
+
     def volume_credits_for(a_performance):
         result = 0
         result += max(a_performance["audience"] - 30, 0)
-        if 'comedy' == play_for(a_performance)['type']:
+        if 'comedy' == a_performance['play']['type']:
             result += a_performance["audience"] // 5
         return result
 
@@ -55,7 +56,7 @@ def render_plain_text(data, invoice, plays):
     result = f'청구 내역 (고객명: {data["customer"]})\n'
 
     for perf in data["performances"]:
-        result += f' {play_for(perf)["name"]}: ${usd(amount_for(perf))} ({perf["audience"]}석)\n'
+        result += f' {perf["play"]["name"]}: ${usd(amount_for(perf))} ({perf["audience"]}석)\n'
 
     result += f'총액: ${usd(total_amount())}\n'
     result += f'적립 포인트: {total_volume_credits()}점'
