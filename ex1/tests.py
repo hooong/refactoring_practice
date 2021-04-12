86c8bcfd015ae2690fd92fe5fe1dc029c907a38e
hooongsj <tjrwns0529@gmail.com>
2021-04-13 01:14:45 +0900

    clean test expected

--- ex1/tests.py
+++ ex1/tests.py
@@ -19,7 +19,12 @@ class StatementTest(unittest.TestCase):
         for invoice in self.invoices:
             result += statement(invoice, self.plays)
 
-        expected = '청구 내역 (고객명: BicCo)\n Hamlet: $650.00 (55석)\n As You Like It: $580.00 (35석)\n Othello: $500.00 (40석)\n총액: $1730.00\n적립 포인트: 47점'
+        expected = '청구 내역 (고객명: BicCo)\n' \
+                   ' Hamlet: $650.00 (55석)\n' \
+                   ' As You Like It: $580.00 (35석)\n' \
+                   ' Othello: $500.00 (40석)\n' \
+                   '총액: $1730.00\n' \
+                   '적립 포인트: 47점'
         self.assertEqual(expected, result)
 
     def test_valid_statement_html(self):
@@ -28,5 +33,12 @@ class StatementTest(unittest.TestCase):
         for invoice in self.invoices:
             result += html_statement(invoice, self.plays)
 
-        expected = '<h1>청구 내역 (고객명: BicCo)</h1>\n<table>\n<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr> <tr><td>Hamlet</td><td>(55석)</td><td>$650.00</td></tr>\n <tr><td>As You Like It</td><td>(35석)</td><td>$580.00</td></tr>\n <tr><td>Othello</td><td>(40석)</td><td>$500.00</td></tr>\n</table>\n<p>총액: <em>$1730.00</em>점</p>\n<p>적립 포인트: <em>$0.47</em>점</p>\n'
+        expected = '<h1>청구 내역 (고객명: BicCo)</h1>\n' \
+                   '<table>\n' \
+                   '<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>' \
+                   ' <tr><td>Hamlet</td><td>(55석)</td><td>$650.00</td></tr>\n' \
+                   ' <tr><td>As You Like It</td><td>(35석)</td><td>$580.00</td></tr>\n' \
+                   ' <tr><td>Othello</td><td>(40석)</td><td>$500.00</td></tr>\n</table>\n' \
+                   '<p>총액: <em>$1730.00</em>점</p>\n' \
+                   '<p>적립 포인트: <em>$0.47</em>점</p>\n'
         self.assertEqual(expected, result)
