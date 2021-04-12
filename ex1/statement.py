a872e89ea466a16ea558867beece31277a350597
hooongsj <tjrwns0529@gmail.com>
2021-04-13 00:08:09 +0900

    change format -> usd

--- ex1/statement.py
+++ ex1/statement.py
@@ -23,8 +23,8 @@ def statement(invoice, plays):
             result += a_performance["audience"] // 5
         return result
 
-    def format(a_number):
-        return f'{a_number:.2f}'
+    def usd(a_number):
+        return f'{a_number/100:.2f}'
 
     total_amount = 0
     volume_credits = 0
@@ -34,9 +34,9 @@ def statement(invoice, plays):
         volume_credits += volume_credits_for(perf)
 
         # 청구 내역 출력
-        result += f' {play_for(perf)["name"]}: ${format(amount_for(perf)/100)} ({perf["audience"]}석)\n'
+        result += f' {play_for(perf)["name"]}: ${usd(amount_for(perf))} ({perf["audience"]}석)\n'
         total_amount += amount_for(perf)
 
-    result += f'총액: ${format(total_amount/100)}\n'
+    result += f'총액: ${usd(total_amount)}\n'
     result += f'적립 포인트: {volume_credits}점'
     return result
