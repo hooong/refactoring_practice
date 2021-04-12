36e4fd5f95a9c6c8613eeee3ebca6183c0e2e9e1
hooongsj <tjrwns0529@gmail.com>
2021-04-13 01:13:22 +0900

    add html_statement

--- ex1/statement.py
+++ ex1/statement.py
@@ -6,9 +6,6 @@ def statement(invoice, plays):
 
 
 def render_plain_text(data):
-    def usd(a_number):
-        return f'{a_number/100:.2f}'
-
     result = f'청구 내역 (고객명: {data["customer"]})\n'
 
     for perf in data["performances"]:
@@ -17,3 +14,24 @@ def render_plain_text(data):
     result += f'총액: ${usd(data["totalAmount"])}\n'
     result += f'적립 포인트: {data["totalVolumeCredits"]}점'
     return result
+
+
+def html_statement(invoice, plays):
+    return render_html(create_statement_data(invoice, plays))
+
+
+def render_html(data):
+    result = f'<h1>청구 내역 (고객명: {data["customer"]})</h1>\n'
+    result += '<table>\n'
+    result += '<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>'
+    for perf in data['performances']:
+        result += f' <tr><td>{perf["play"]["name"]}</td><td>({perf["audience"]}석)</td>'
+        result += f'<td>${usd(perf["amount"])}</td></tr>\n'
+    result += '</table>\n'
+    result += f'<p>총액: <em>${usd(data["totalAmount"])}</em>점</p>\n'
+    result += f'<p>적립 포인트: <em>${usd(data["totalVolumeCredits"])}</em>점</p>\n'
+    return result
+
+
+def usd(a_number):
+    return f'{a_number/100:.2f}'
--- ex1/tests.py
+++ ex1/tests.py
@@ -1,7 +1,7 @@
 import json
 import unittest
 
-from ex1.statement import statement
+from ex1.statement import statement, html_statement
 
 
 class StatementTest(unittest.TestCase):
@@ -17,7 +17,16 @@ class StatementTest(unittest.TestCase):
         result = ''
 
         for invoice in self.invoices:
-            result = statement(invoice, self.plays)
+            result += statement(invoice, self.plays)
 
         expected = '청구 내역 (고객명: BicCo)\n Hamlet: $650.00 (55석)\n As You Like It: $580.00 (35석)\n Othello: $500.00 (40석)\n총액: $1730.00\n적립 포인트: 47점'
+        self.assertEqual(expected, result)
+
+    def test_valid_statement_html(self):
+        result = ''
+
+        for invoice in self.invoices:
+            result += html_statement(invoice, self.plays)
+
+        expected = '<h1>청구 내역 (고객명: BicCo)</h1>\n<table>\n<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr> <tr><td>Hamlet</td><td>(55석)</td><td>$650.00</td></tr>\n <tr><td>As You Like It</td><td>(35석)</td><td>$580.00</td></tr>\n <tr><td>Othello</td><td>(40석)</td><td>$500.00</td></tr>\n</table>\n<p>총액: <em>$1730.00</em>점</p>\n<p>적립 포인트: <em>$0.47</em>점</p>\n'
         self.assertEqual(expected, result)
