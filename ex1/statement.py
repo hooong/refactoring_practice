9f074658ee00fd51f022ed544f14ee7e3edaf43b
hooongsj <tjrwns0529@gmail.com>
2021-04-12 23:55:18 +0900

    inline this_amount

--- ex1/statement.py
+++ ex1/statement.py
@@ -21,10 +21,6 @@ def statement(invoice, plays):
     result = f'청구 내역 (고객명: {invoice["customer"]})\n'
 
     for perf in invoice["performances"]:
-        this_amount = 0
-
-        this_amount = amount_for(perf)
-
         # 포인트 적립
         volume_credits += max(perf["audience"] - 30, 0)
         # 희극 관객 5명당 추가 포인트 제공
@@ -32,8 +28,8 @@ def statement(invoice, plays):
             volume_credits += perf["audience"] // 5
 
         # 청구 내역 출력
-        result += f' {play_for(perf)["name"]}: ${(this_amount/100):.2f} ({perf["audience"]}석)\n'
-        total_amount += this_amount
+        result += f' {play_for(perf)["name"]}: ${( amount_for(perf)/100):.2f} ({perf["audience"]}석)\n'
+        total_amount +=  amount_for(perf)
 
     result += f'총액: ${(total_amount/100):.2f}\n'
     result += f'적립 포인트: {volume_credits}점'
