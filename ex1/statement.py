5a6792c59673b8e93d8318d946e47dea2732995d
hooongsj <tjrwns0529@gmail.com>
2021-04-12 23:52:22 +0900

    inline play

--- ex1/statement.py
+++ ex1/statement.py
@@ -14,27 +14,25 @@ def statement(invoice, plays):
         return result
 
     def play_for(a_performance):
-        play = plays[a_performance["playID"]]
-        return play
+        return plays[a_performance["playID"]]
 
     total_amount = 0
     volume_credits = 0
     result = f'청구 내역 (고객명: {invoice["customer"]})\n'
 
     for perf in invoice["performances"]:
-        play = play_for(perf)
         this_amount = 0
 
-        this_amount = amount_for(perf, play)
+        this_amount = amount_for(perf, play_for(perf))
 
         # 포인트 적립
         volume_credits += max(perf["audience"] - 30, 0)
         # 희극 관객 5명당 추가 포인트 제공
-        if 'comedy' == play['type']:
+        if 'comedy' == play_for(perf)['type']:
             volume_credits += perf["audience"] // 5
 
         # 청구 내역 출력
-        result += f' {play["name"]}: ${(this_amount/100):.2f} ({perf["audience"]}석)\n'
+        result += f' {play_for(perf)["name"]}: ${(this_amount/100):.2f} ({perf["audience"]}석)\n'
         total_amount += this_amount
 
     result += f'총액: ${(total_amount/100):.2f}\n'
