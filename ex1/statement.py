a0db0d54f16e78bfedb573328302eb0f345a8b33
hooongsj <tjrwns0529@gmail.com>
2021-04-13 00:44:34 +0900

    delete amount from render_plain_text

--- ex1/statement.py
+++ ex1/statement.py
@@ -5,18 +5,12 @@ def statement(invoice, plays):
     def enrich_performance(a_performance):
         result = copy.copy(a_performance)
         result['play'] = play_for(result)
+        result['amount'] = amount_for(result)
         return result
 
     def play_for(a_performance):
         return plays[a_performance["playID"]]
 
-    statement_data = {}
-    statement_data['customer'] = invoice['customer']
-    statement_data['performances'] = list(map(enrich_performance, invoice['performances']))
-    return render_plain_text(statement_data)
-
-
-def render_plain_text(data):
     def amount_for(a_performance):
         if a_performance['play']['type'] == 'tragedy':
             result = 40000
@@ -31,6 +25,13 @@ def render_plain_text(data):
             raise Exception(f'알 수 없는 장르 : {a_performance["play"]["type"]}')
         return result
 
+    statement_data = {}
+    statement_data['customer'] = invoice['customer']
+    statement_data['performances'] = list(map(enrich_performance, invoice['performances']))
+    return render_plain_text(statement_data)
+
+
+def render_plain_text(data):
     def volume_credits_for(a_performance):
         result = 0
         result += max(a_performance["audience"] - 30, 0)
@@ -47,7 +48,7 @@ def render_plain_text(data):
     def total_amount():
         result = 0
         for perf in data["performances"]:
-            result += amount_for(perf)
+            result += perf['amount']
         return result
 
     def usd(a_number):
@@ -56,7 +57,7 @@ def render_plain_text(data):
     result = f'청구 내역 (고객명: {data["customer"]})\n'
 
     for perf in data["performances"]:
-        result += f' {perf["play"]["name"]}: ${usd(amount_for(perf))} ({perf["audience"]}석)\n'
+        result += f' {perf["play"]["name"]}: ${usd(perf["amount"])} ({perf["audience"]}석)\n'
 
     result += f'총액: ${usd(total_amount())}\n'
     result += f'적립 포인트: {total_volume_credits()}점'
